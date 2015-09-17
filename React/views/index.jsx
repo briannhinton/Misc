308d7ce7c0cb5825ba8fd12112f8198e2cc4ec05
Brian <briannhinton@gmail.com>
2015-09-17 09:52:33 -0400

    Added props

--- React/views/index.jsx
+++ React/views/index.jsx
@@ -2,22 +2,38 @@ var React = require('react');
 
 var TodoBox = React.createClass({
   render: function() {
-    return (
-      <div className="todoBox">
-        <h1>Todos</h1>
-        <TodoList />
-        <TodoForm />
-      </div>
-    );
+      return (
+        <div className="todoBox">
+          <h1>Todos</h1>
+	         <TodoList />
+           <TodoForm />
+        </div>
+      );
   }
 });
 
 var TodoList = React.createClass({
   render: function() {
     return (
-      <div className="todoList">
-        I am a TodoList.
+      <div className = "todoList">
+        <table style={{border: "2px solid black"}}>
+  	       <tbody>
+            <Todo title="Shopping">Milk</Todo>
+	          <Todo title="Hair cut">13:00</Todo>
+          </tbody>
+	       </table>
       </div>
+    );
+  }
+});
+
+var Todo = React.createClass({
+  render: function() {
+    return (
+      <tr>
+        <td style={{border: "1px solid black"}}>{this.props.title}</td>
+        <td style={{border: "1px solid black"}}>{this.props.children}</td>
+      </tr>
     );
   }
 });
@@ -25,7 +41,7 @@ var TodoList = React.createClass({
 var TodoForm = React.createClass({
   render: function() {
     return (
-      <div className="todoForm">
+      <div className = "todoForm">
         I am a TodoForm.
       </div>
     );
